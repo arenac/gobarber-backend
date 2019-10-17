21d7b52fb9f33f0109b1a52b2cc22b8e8242f72b
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-17 23:23:00 +0200

    database config and add model User

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
+import User from './app/models/User';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello world!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Nilo Neregato',
+    email: 'neregato.nilo@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
