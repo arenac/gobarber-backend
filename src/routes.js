91a4a5c4562e1b9fa7dccf25bf42c81edf7a0323
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-17 23:42:59 +0200

    add users post

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
-import User from './app/models/User';
+import { Router } from 'express';
 
-const { Router } = require('express');
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Nilo Neregato',
-    email: 'neregato.nilo@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
