0cdcb666e8b197f226737ee6f2880c96e85abf20
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-19 02:42:17 +0200

    add yup validation

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,7 +7,7 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return req.status(401).json({ error: 'Token not provided' });
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
   const [, token] = authHeader.split(' ');
@@ -18,9 +18,7 @@ export default async (req, res, next) => {
     req.userId = decoded.id;
 
     return next();
-  } catch (error) {
-    return req.status(401).json({ error: 'Token invalid' });
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
   }
-
-  return next();
 };
