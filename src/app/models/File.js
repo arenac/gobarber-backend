80db643663870fdfe44bffb2fdf2a4bf29167c45
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-20 22:31:54 +0200

    add providers controller with index

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`; // to work in a page or app needs the ip instead of 'localhost'
+          },
+        },
       },
       {
         sequelize,
