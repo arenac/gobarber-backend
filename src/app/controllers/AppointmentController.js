a7dd4dd512224b71eb479c25540d9c810e5eae11
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-21 23:12:28 +0200

    add send email notification

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async store(req, res) {
@@ -109,7 +110,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res
@@ -128,6 +137,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Appointment canceled',
+      text: 'A new appointment was caceled',
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'dff9801cf41c60',
+    pass: '25596834ba0e9c',
+  },
+  default: {
+    from: 'GoBarber team <noreplay@gobarber.com>',
+  },
+};
+
+/**
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * Mailtrap (DEV)
+ */
