336e5be5f4dd2027686aef57fc78ad9957e99689
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-21 23:53:31 +0200

    optimize send email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -143,7 +143,7 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
+    Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Appointment canceled',
       template: 'cancelation',
